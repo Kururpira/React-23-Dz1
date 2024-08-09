726d1a5536e5ca65b02edcf422238adc2fe1d6e3
Kururpira <kururpira@inbox.ru>
2024-08-09 17:00:12 +0300

    добавить стиль для карточки тарифа за 550

--- src/Components/card300/Card300.jsx
+++ src/Components/card300/Card300.jsx
@@ -2,16 +2,18 @@
 import Card from '../../Card';
 import './Card300.css'
 
+
 function Card300(){
     return(
     <Card
         title="Безлимитный 300"
-        rate="руб 300/мес"
+        rate= "руб 300/мес"
         description="До 10 Мбит/сек"
-        descriptionheader="Объем включенного трафика
-        не ограничен"
+        descriptionheader="Объем включенного трафика не ограничен"
     />
     );
     }
+
+
     
     export default Card300;
